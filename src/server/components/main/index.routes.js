054b93fb4dcae9294d015de8c067f4aaf3f72525
nick <nickwhite917@yahoo.com>
2016-08-31 15:06:19 -0400

    Restructure app for feature based development

--- src/server/components/main/index.routes.js
+++ src/server/components/main/index.routes.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-var Product = require('../models/registration.js');
+var Product = require('../registration/registration.model.js');
 
 
 router.get('/', function(req, res, next) {
-  return res.render('index', {
+  return res.render(__dirname + '/main/index.view', {
     user: req.user,
     message: req.flash('message')[0]
   });
